9b6540b83f43630567377a7191555b0f3a3e5888
unknown <dennednonato@gmail.com>
2025-05-07 15:55:47 -0300

    primeiro commit

---
+++ assets/js/nav.js
@@ -0,0 +1,16 @@
+const header = document.querySelector("header");
+
+window.addEventListener ("scroll", function() {
+    header.classList.toggle ("sticky", window.scrollY > 40)
+});
+
+let menu = document.querySelector('#menu-icon');
+let navlist = document.querySelector('.navlist');
+
+menu.onclick = () => {
+    navlist.classList.toggle('open');
+    if(window.scrollY < 90){
+        header.classList.toggle ("sticky");
+    }
+    
+}
---
+++ assets/js/top-button.js
@@ -0,0 +1,15 @@
+window.onscroll = () => {
+    menu.classList.remove('bx-x');
+    navlist.classList.remove('open');
+}
+
+const topo = document.getElementById("top");
+
+window.addEventListener("scroll", function() {
+    topo.classList.toggle ("show-top", window.scrollY > 140)
+    topo.classList.toggle ("hide-top", window.scrollY < 140);
+});
+
+window.onload = () => {
+    topo.classList.toggle ("hide-top");
+}
